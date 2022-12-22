e114b3ff6c51849c5892a987057c3b616414ffdc
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-22 13:00:00 +0000

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from './components/functionalGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreeting />
+    </div>
+  );
+}
+
+export default App;
